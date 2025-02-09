af064ac47fb2d4fe2b544206ba17f5bdcd28118e
Kambojharsh <kambojharsh2@gmail.com>
2025-02-09 22:09:19 +0530

    removed old authgaurd commentted

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,38 +1,3 @@
-// import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-// import { JwtService } from '@nestjs/jwt';
-// import { Observable } from 'rxjs';
-
-// @Injectable()
-// export class JwtAuthGuard implements CanActivate {
-//   constructor(private jwtService: JwtService) {}
-
-//   canActivate(
-//     context: ExecutionContext,
-//   ): boolean | Promise<boolean> | Observable<boolean> {
-//     const request = context.switchToHttp().getRequest();
-//     const authHeader = request.headers['authorization'] || request.headers['Authorization'];
-
-//     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-//       console.error('Missing or malformed token');
-//       return false;
-//     }
-
-//     const token = authHeader.split(' ')[1];
-
-//     console.log('Auth Header:', authHeader);
-//     console.log('Token:', token);
-
-//     try {
-//       const payload = this.jwtService.verify(token);
-//       request.user = payload;
-//       return true;
-//     } catch (error) {
-//       console.error('JWT verification failed:', error);
-//       return false;
-//     }
-//   }
-// }
-
 import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
