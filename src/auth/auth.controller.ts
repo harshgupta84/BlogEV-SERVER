5e869667ce9178094e4bdcba3e06b77c09f6d4ec
Harsh G <harsh.g@eagleview.com>
2025-02-09 22:05:01 +0530

    DTO in auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignupDto } from './dto/signup.dto';
+import { SigninDto } from './dto/signin.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  async signUp(@Body() body: { name: string; email: string; password: string }) {
+  async signUp(@Body() body: SignupDto) {
     return this.authService.signUp(body.name, body.email, body.password);
   }
 
   @Post('signin')
-  async signIn(@Body() body: { email: string; password: string }) {
+  async signIn(@Body() body: SigninDto) {
     return this.authService.signIn(body.email, body.password);
   }
 }
